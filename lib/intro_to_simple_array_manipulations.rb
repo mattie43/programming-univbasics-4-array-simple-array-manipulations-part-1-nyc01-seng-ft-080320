343c64c76e35bf86f8046d4b6063da3170c90f2f
Matt Ericksen <ericksenm93@ymail.com>
2020-07-21 23:34:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_pop(arr)
 end
 
 def pop_with_args(arr)
-  arr.pop[2]
+  arr.pop(2)
 end
 
 def using_shift(arr)
@@ -19,5 +19,5 @@ def using_shift(arr)
 end
 
 def shift_with_args(arr)
-  arr.shift[2]
+  arr.shift(2)
 end
