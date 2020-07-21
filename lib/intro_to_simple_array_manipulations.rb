b5a2766145021f73a7db870f5898f3e90f07f9bf
Matt Ericksen <ericksenm93@ymail.com>
2020-07-21 23:32:30 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_push(arr, str)
+  arr.push(str)
+end
