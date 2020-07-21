a3fa42d15c64795a769299de3378d62e75ea2740
Matt Ericksen <ericksenm93@ymail.com>
2020-07-21 23:34:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,10 +6,18 @@ def using_unshift(arr, str)
   arr.unshift(str)
 end
 
-def using_pop(arr, str)
-  arr.pop(str)
+def using_pop(arr)
+  arr.pop
 end
 
-def using_push(arr, str)
-  arr.push(str)
+def pop_with_args(arr)
+  arr.pop[2]
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift[2]
 end
